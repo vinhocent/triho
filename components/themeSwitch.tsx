1a1b6dea51c650df2dd0d9cb62319a43737b8ff0
Tri Ho <vinhocent@gmail.com>
2022-11-13 05:08:17 -0500

    fixed some theme-ing

--- components/themeSwitch.tsx
+++ components/themeSwitch.tsx
@@ -1,80 +1,45 @@
-import { useState, useEffect } from 'react'
-import { useTheme } from 'next-themes'
-import {MoonIcon, SunIcon} from '@heroicons/react/solid'
-
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { MoonIcon, SunIcon } from "@heroicons/react/solid";
 
 const ThemeSwitch = () => {
-  const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
   // check and reset theme
-  const themeCheck = () => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      setTheme('dark');
-    } else {
-      setTheme('light');
-    }
-  }
-
 
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
-    themeCheck();
-    setMounted(true)
+    setMounted(true);
   }, [theme]);
 
   // check theme on component mount
   useEffect(() => {
-    themeCheck();
-    setMounted(true)
-
+    setMounted(true);
   }, []);
 
   if (!mounted) {
-    return null
+    return null;
   }
-
 
   const renderLogo = () => {
-    if (theme === 'dark'){
-      return(
-        <SunIcon 
-        className = "w-5 h-5"          
-          />
-  
-      )
+    if (theme === "dark") {
+      return <SunIcon className="w-5 h-5" />;
     } else {
-      return(
-        <MoonIcon   
-        className = "w-5 h-5"          
- 
-          />
-      )
-
-
+      return <MoonIcon className="w-5 h-5" />;
     }
-
-  }
-
+  };
 
   return (
-         <button
-            aria-label="Toggle Dark Mode"
-            type="button"
-            onClick={() =>
-              setTheme(theme === 'dark' ? 'light' : 'dark')}
-            className=" w-9 h-9 bg-amber-100 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all" >
-              {renderLogo()}
-
-            </button>
-
-     
-      
-  )
-}
+    <button
+      aria-label="Toggle Dark Mode"
+      type="button"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      className=" w-9 h-9 bg-amber-100 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all"
+    >
+      {renderLogo()}
+    </button>
+  );
+};
 
 export default ThemeSwitch;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import "../styles/globals.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import type { AppProps } from "next/app";
 import { useTheme, ThemeProvider } from "next-themes";
 
@@ -7,6 +7,22 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { theme, setTheme } = useTheme();
+  const themeCheck = () => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
+  useEffect(() => {
+    themeCheck();
+  }, []);
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <Header />
