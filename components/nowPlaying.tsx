614b50534a612b8847cb0c1a7ce1194248e3934b
Tri Ho <vinhocent@gmail.com>
2022-11-14 23:33:12 -0500

    fixed deployment error

--- components/nowPlaying.tsx
+++ components/nowPlaying.tsx
@@ -101,7 +101,7 @@ export default function NowPlaying() {
           </a>
         ) : (
           <p className="capsize text-gray-500 dark:text-gray-400 font-medium">
-            What I'm Listening To
+            What I`&aposm Listening To
           </p>
         )}
       </div>
