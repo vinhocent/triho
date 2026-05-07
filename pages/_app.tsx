18d9d19bf68066865bed154e806ce12666e952d2
Tri Ho <vinhocent@gmail.com>
2026-05-07 15:08:34 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
         </style>
       </Head>
       <Component {...pageProps} />
+      <link rel="icon" href="/favicon.ico" />
 
       <Footer />
     </ThemeProvider>
